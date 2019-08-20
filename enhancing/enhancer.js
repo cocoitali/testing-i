d85bdd5092b521a9221a326c7a1ae0fc76478deb
cocoitali <cmburatto@gmail.com>
2019-08-19 19:16:40 -0600

    Set up starter code for project

---
+++ enhancing/enhancer.js
@@ -0,0 +1,23 @@
+module.exports = {
+    succeed,
+    fail,
+    repair,
+    get,
+
+};
+
+const succeed = (item) => {
+    return { ...item }
+};
+
+const fail = (item) => {
+    return { ...item}
+}
+
+const repair = (item) => {
+    return { ...item}
+}
+
+const get = (item) => {
+    return { ...item}
+}
---
+++ enhancing/enhancer.spec.js
@@ -0,0 +1,20 @@
+const enhancer = require("./enhancer.js");
+//test away!
+
+describe("the enhancer", () => {
+  describe("the repair function", () => {
+    it("should restore the durability", () => {
+        // arrange : set up any mock data we might use
+        const longsword = {};
+
+        //act : pass things into the function we want to test
+        const result = enhancer.repair(longsword);
+
+        //assert : make any assertions here
+        //add code here <---------------
+    });
+  });
+});
+
+
+//****** hint: you will have to make mock data in order to test these functions correctly */
